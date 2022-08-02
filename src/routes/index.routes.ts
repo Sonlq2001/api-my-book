73a15fee3a204fee61f4ff2d9df23b8e26fc37e3
sonel <sonweb2001@gmail.com>
2022-08-03 06:49:06 +0700

    upload file on  driver

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
 import AuthRoute from "./auth.routes";
+import BookRoute from "./book.routes";
 
-export const routes: Router[] = [AuthRoute];
+export const routes: Router[] = [AuthRoute, BookRoute];
