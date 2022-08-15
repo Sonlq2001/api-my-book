ecf778880bcceff3736a60566edf177336f95dc5
mvn-sonle2-hn <son.le2@monstar-lab.com>
2022-08-15 20:36:33 +0700

    BE: api book v2

--- src/routes/book.routes.ts
+++ src/routes/book.routes.ts
@@ -1,9 +1,23 @@
 import express from "express";
 
-import { createBook } from "../controllers/bookController";
+import {
+  createBook,
+  getBookPendingOrDone,
+  updateScheduleBook,
+  getBook,
+  getBookBeingRead,
+} from "../controllers/bookController";
 
 const route = express.Router();
 
 route.post("/book", createBook);
+
+route.get("/books", getBookPendingOrDone);
+
+route.patch("/schedule-book/:id", updateScheduleBook);
+
+route.get("/book/:id", getBook);
+
+route.get("/book-being", getBookBeingRead);
 
 export default route;
--- src/routes/file.routes.ts
+++ src/routes/file.routes.ts
@@ -1,27 +1,27 @@
 import express from "express";
 import multer from "multer";
 import path from "path";
-import createError from 'http-errors';
+import createError from "http-errors";
 
 import { uploadFileDiver } from "../controllers/fileController";
 
 const storage = multer.diskStorage({
-	destination: function (req, file, cb) {
-		cb(null, `${path.join(__dirname, "../assets/uploads/")}`);
-	},
-	filename: function (req, file, cb) {
-		cb(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
-	},
+  destination: function (req, file, cb) {
+    cb(null, `${path.join(__dirname, "../assets/uploads/")}`);
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+  },
 });
 
-const upload = multer({ 
-	storage,  
-	fileFilter: (req, file, cb) => {
-		if(!["image/jpg", "image/png", "image/jpeg"].includes(file.mimetype)){
-			return cb((new createError.InternalServerError("Incorrect file format")));
-		}
-		cb(null, true)
-	}
+const upload = multer({
+  storage,
+  fileFilter: (req, file, cb) => {
+    if (!["image/jpg", "image/png", "image/jpeg"].includes(file.mimetype)) {
+      return cb(new createError.InternalServerError("Incorrect file format"));
+    }
+    cb(null, true);
+  },
 });
 
 const route = express.Router();
